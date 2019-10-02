8f5c5b4117898d6695b0abb7440e4f5a0502d3a3
roydemirjian <roykebab@gmail.com>
2019-10-02 19:12:24 -0400

    Fixed some stuff

--- login.php
+++ login.php
@@ -6,28 +6,36 @@
 if(isset($_GET["email"]) && !empty($_GET["email"])){
     if(strpos(($_GET["email"]),'@')){
         $email = $_GET["email"];
+        echo "Email is: " . $email;
+        echo "<br><br>";
         }else{
-            echo "Email must be valid";
+            echo nl2br("Email must be valid");
+            echo "<br><br>";
         }
     }else{
-        echo "Email Required";
+        echo nl2br("Email Required");
+        echo "<br><br>";
 }
 
 if(isset($_GET["password"]) && !empty($_GET["password"])){
-    if(strlen($_GET["password"])>8){
+    if(strlen($_GET["password"])>7){
         $password = $_GET["password"];
+        echo "Password is: " . $password;
+        echo "<br><br>";
         }else{
-            echo "Password length must be 8 characters";
+            echo nl2br("Password length must be 8 characters");
+            echo "<br><br>";
         }
     }else{
-        echo "Password Required";
+        echo nl2br("Password Required");
+        echo "<br><br>";
 }
 
 
 
-echo "Email is: " . $email;
-echo "<br>";
-echo "Password is: " . $password;
+#echo "Email is: " . $email;
+#echo "<br><br>";
+#echo "Password is: " . $password;
 
 
 ?>
